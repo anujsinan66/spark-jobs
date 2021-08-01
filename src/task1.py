470256191f63992e8129ed4839743d8834842ce9
anujsingh <anujsinghdeveloper@gmail.com>
2021-08-01 13:13:10 +0100

    in this commit: added new branch dev

--- src/task1.py
+++ src/task1.py
@@ -2,8 +2,7 @@ from pyspark.sql import SparkSession
 spark:SparkSession = SparkSession.builder.master("local[*]").appName("task1").getOrCreate()
 csvRdd = spark.sparkContext.textFile("../input/groceries.csv").map(lambda line: line.split(",")).take(5)
 for data in csvRdd:
-       print('hello')
-       print('hello2')
+       print(data)
        print('hello')
 
 
