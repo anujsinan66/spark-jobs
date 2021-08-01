7ec66626d996760969244c4bbaf96353adc7572d
anujsinan66 <79793168+anujsinan66@users.noreply.github.com>
2021-08-01 13:13:35 +0100

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -8,7 +8,7 @@ def write_file(count):
 spark:SparkSession = SparkSession.builder.master("local[*]").appName("task1").getOrCreate()
 csvRdd = spark.sparkContext.textFile("../input/groceries.csv").flatMap(lambda line: line.split(",")).distinct()
 write_file(csvRdd.count())
-
+print('hello')
 
 
 
